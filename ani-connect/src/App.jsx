485d3ae8ad22b2d93894dabb7605e7c2d20c5126
Nitingowda20 <Nitingowda3448@gmail.com>
2024-08-07 16:42:20 +0530

    first commit

--- ani-connect/src/App.jsx
+++ ani-connect/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignInPage from './pages/SignInPage'
+import SignOutPage from './pages/SignOutPage'
+import AboutPage from './pages/AboutPage'
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-4xl text-blue-800'>App haaaaaaaain</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/sign-in' element={<SignInPage />}></Route>
+        <Route path='/sign-out' element={<SignOutPage />}></Route>
+        <Route path='/about' element={<AboutPage />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/projects' element={<Projects />}></Route>
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ ani-connect/src/pages/SignOutPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignOutPage() {
+  return (
+    <div>
+      OUTtttttttttttttttt
+    </div>
+  )
+}
